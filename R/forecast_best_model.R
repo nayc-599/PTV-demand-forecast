c555edbae92687b279f03348c04152c49ecc6c96
Nay Chi <naychi1301@gmail.com>
2026-02-02 21:51:18 +1100

    modularise

--- R/forecast_best_model.R
+++ R/forecast_best_model.R
@@ -1,4 +1,4 @@
-forecast_best_model <- function(mode, h){
+remove_outliers <- function(mode){
   monthly_data <- mode$data
   ts_full <- ts(monthly_data$Pax, start = c(2018, 1), frequency = 12)
 
@@ -12,26 +12,10 @@ forecast_best_model <- function(mode, h){
   ts_clean <- ts_full
   ts_clean[outlier_full == 1] <- NA
 
-  train <- window(ts_clean, end = c(2023, 9))
-  test <- window(ts_clean, start = c(2023, 10))
-
-  models <- train_models(train, outlier_full[1:length(train)])
-  rmse_vals <- evaluate_models(models, test)
-
-  best <- best_model(rmse_vals)
-
-  best_fit_full <- switch(best, ETS = ets(ts_clean), TBATS = tbats(ts_clean))
-
-  fc_best <- forecast(best_fit_full, h = h)
-  fc_test <- forecast(models[[best]], h = length(test))
-  acc <- accuracy(fc_test, test)
-
-  return(list(forecast = fc_best, best_model = best, ts_full = ts_full, 
-                accuracy = acc))
-
+  return (ts_clean)
 }
 
-train_models <- function(train, outlier) {
+train_models <- function(train) {
   fit_ets <- ets(train, model = "ZZZ")
   
   fit_tbats <- tbats(train)
@@ -47,14 +31,35 @@ evaluate_models <- function(models, test) {
 
   fc_tbats <- forecast(models$TBATS, h = length(test))
   rmse_tbats <- accuracy(fc_tbats, test)[2, "RMSE"]
-
-  rmse_values <- c(ETS = rmse_ets, TBATS = rmse_tbats)
-
-  return (rmse_values)
+  
+  return(c(ETS = rmse_ets, TBATS = rmse_tbats))
 }
 
 best_model <- function(rmse_values) {
   best <- names(rmse_values)[which.min(rmse_values)]
   return (best)
+}
+
+forecast_best_model <- function(mode, h){
+  ts_clean <- remove_outliers(mode)
+
+  train <- window(ts_clean, end = c(2023, 9))
+  test <- window(ts_clean, start = c(2023, 10))
+
+  models <- train_models(train)
+  rmse_vals <- evaluate_models(models, test)
+
+  best <- best_model(rmse_vals)
+
+  best_fit_full <- switch(best,
+                            ETS = ets(ts_clean),
+                            TBATS = tbats(ts_clean))
+  fc_best <- forecast(best_fit_full, h = h)
+  fc_test <- forecast(models[[best]], h = length(test))
+  acc <- accuracy(fc_test, test)
+
+  return(list(forecast = fc_best, best_model = best, ts_full = ts_full,
+                accuracy = acc))
+
 }
 
